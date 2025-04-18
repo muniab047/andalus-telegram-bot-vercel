87c4f42de34d9d4d0b28ebbb87393e82304071e1
Nasiha Abdella <muniab047@gmail.com>
2025-04-18 16:56:47 +0300

    debugging

--- index.py
+++ index.py
@@ -30,7 +30,7 @@ query_handler = QueryHandler()
 
 
 
-message_handler = TelegramHandlers(bot_state, bot_state_machine, query_handler, config)
+telegram_handler = TelegramHandlers(bot_state, bot_state_machine, query_handler, config)
 # SQLAlchemy session maker
 def start_session():
     engine = create_engine(DB_URI, client_encoding="utf8")
@@ -63,12 +63,10 @@ class TelegramWebhook(BaseModel):
 
 
 def register_application(application):
-    # application.add_handler(CommandHandler("start", handler.start))
-    # application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handler.message_handler))
-    # application.add_handler(CallbackQueryHandler(handler.query_handler))
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, button_handler))
-    application.add_handler(CallbackQueryHandler(button_click))
+    application.add_handler(CommandHandler("start", telegram_handler.start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, telegram_handler.message_handler))
+    application.add_handler(CallbackQueryHandler(telegram_handler.query_handler))
+    
 
 
 @app.post("/webhook")
